ec69a3d8344758f47b3f2f9f98c0efbc3e50d2d4
PAKAJl <johndoe@example.com>
2020-12-05 14:11:30 +0300

    Tomato Class fixed

--- EPAM-Cource-1/Models/VegetModels/Tomato.cs
+++ EPAM-Cource-1/Models/VegetModels/Tomato.cs
@@ -7,11 +7,8 @@ namespace EPAM_Cource_1.Models.VegetModels
     class Tomato : IVegetable
     {
         public int Calories { get; } = 200;
-
         public float Protein { get; } = 1.2f;
-
         public float Fats { get; } = 7.4f;
-
         public float Carbohydrates { get; } = 5.3f;
         public VegetType Type { get; } = VegetType.Tomato;
     }
